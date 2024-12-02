3c9405539d798b86c16dcd45bfd42db008193001
FlorianSchatz <st191622@stud.uni-stuttgart.de>
2024-12-02 16:49:28 +0100

    Changed comments in main

--- human_activity/main.py
+++ human_activity/main.py
@@ -13,22 +13,19 @@ FLAGS = flags.FLAGS
 flags.DEFINE_boolean('train', True, 'Specify whether to train or evaluate a model.')
 
 def main(argv):
-    # gin-config
+    # collect data from gin config file
     gin.parse_config_files_and_bindings(['configs/config.gin'], [])
 
-
-    # generate folder structures
+    # generate folder structures for all model and logging data
     run_paths = utils_params.gen_run_folder()
 
-    # set loggers
+    # set logger for training
     utils_misc.set_loggers(run_paths['path_logs_train'], logging.INFO)
 
     # set config path
     utils_params.save_config(run_paths['path_gin'], gin.config_str())
 
-
-
-    # setup pipeline
+    # call of data pipeline to retrieve train, validation and test dataset
     ds_train, ds_val, ds_test, ds_info = datasets.load()
 
     # step for single class per sequence (sequence2label)
@@ -36,10 +33,11 @@ def main(argv):
     ds_val = ds_val.map(lambda x, y: (x, tf.reduce_mean(y, axis=1)))
     ds_test = ds_test.map(lambda x, y: (x, tf.reduce_mean(y, axis=1)))
 
-    # model
+    # create model
     model = architectures.lstm_architecture(input_shape=(250,6), n_classes=12)
 
     if FLAGS.train:
+        # initialize Trainer class based on given model and datasets
         trainer = Trainer(model=model, ds_train=ds_train, ds_val=ds_val, learning_rate=0.00001, run_paths=run_paths)
         for _ in trainer.train():
             continue
