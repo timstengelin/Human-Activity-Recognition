ad4923ba68cbfb2c58c1d4aa38e34068c5274e07
FlorianSchatz <st191622@stud.uni-stuttgart.de>
2024-12-06 16:51:49 +0100

    Full LSTM forward model

--- human_activity/models/architectures.py
+++ human_activity/models/architectures.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import models.layers as custom_layers
 
 @gin.configurable
-def lstm_architecture(input_shape, n_classes):
+def lstm_architecture(input_shape, n_classes, dropout_rate):
     """A basic LSTM architecture
 
         Parameters:
@@ -17,6 +17,14 @@ def lstm_architecture(input_shape, n_classes):
     inputs = tf.keras.Input(input_shape)
     out = custom_layers.basic_lstm_layer(inputs=inputs, units=250, return_sequences=True)
     out = custom_layers.basic_dense_layer(inputs=out, units=125)
-    out = custom_layers.basic_lstm_layer(inputs=out, units=64, return_sequences=False)
-    out = custom_layers.basic_dense_layer(inputs=out, units=n_classes, activation="sigmoid")
-    return tf.keras.Model(inputs=inputs, outputs=out, name='lstm_base')
+    out = tf.keras.layers.Dropout(dropout_rate)(out)
+    out = custom_layers.basic_lstm_layer(inputs=inputs, units=125, return_sequences=True)
+    out = custom_layers.basic_dense_layer(inputs=out, units=64)
+    out = tf.keras.layers.Dropout(dropout_rate)(out)
+    out = custom_layers.basic_lstm_layer(inputs=out, units=64, return_sequences=True)
+    out = custom_layers.basic_dense_layer(inputs=out, units=32)
+    out = tf.keras.layers.Dropout(dropout_rate)(out)
+
+    out = custom_layers.basic_dense_layer(inputs=out, units=n_classes, activation="softmax")
+
+    return tf.keras.Model(inputs=inputs, outputs=out, name='LSTM_model')S
