502d6554045f1bff85af30e71a59c56cf86fc820
Tim Stengelin <st190770@stud.uni-stuttgart.de>
2024-12-09 17:46:57 +0100

    Added functionality for confusion matrix

--- diabetic_retinopathy/evaluation/metrics.py
+++ diabetic_retinopathy/evaluation/metrics.py
@@ -1,13 +1,25 @@
 import tensorflow as tf
 
-class ConfusionMatrix(tf.keras.metrics.Metric):
+class BinaryConfusionMatrix(tf.keras.metrics.Metric):
 
-    def __init(self, name="confusion_matrix", **kwargs):
-        super(ConfusionMatrix, self).__init__(name=name, **kwargs)
-        # ...
+    def __init(self, name='binary_confusion_matrix', **kwargs):
+        super(BinaryConfusionMatrix, self).__init__(name=name, **kwargs)
+        self.binary_confusion_matrix = self.add_weight(name='binary_confusion_matrix', shape=(2, 2), initializer='zeros')
 
-    def update_state(self, *args, **kwargs):
-        # ...
+    def update_state(self, labels, predictions, *args, **kwargs):
+        # Convert labels to integer type for consistency
+        labels = tf.cast(labels, dtype=tf.int32)
+
+        # Convert predictions to a 1D array of predicted class indices
+        predictions = tf.reshape(tf.math.argmax(predictions, axis=1), shape=(-1, 1))
+        predictions = tf.cast(predictions, dtype=tf.int32)
+
+        # Compute the confusion matrix and update the state
+        self.binary_confusion_matrix= tf.math.confusion_matrix(
+            tf.squeeze(labels),
+            tf.squeeze(predictions),
+            num_classes=2,
+            dtype=tf.int32)
 
     def result(self):
-        # ...
+        return self.binary_confusion_matrix
