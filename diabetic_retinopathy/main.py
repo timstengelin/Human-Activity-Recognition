cc6f5dc9f08c5cbbdfa439641cf2a3ae649e3c38
Tim Stengelin <st190770@stud.uni-stuttgart.de>
2024-12-09 16:07:53 +0100

    Changed minor things in main.py

--- diabetic_retinopathy/main.py
+++ diabetic_retinopathy/main.py
@@ -6,7 +6,7 @@ from train import Trainer
 from evaluation.eval import evaluate
 from input_pipeline import datasets
 from utils import utils_params, utils_misc
-from models.architectures import vgg_like
+from models.architectures import le_net, vgg16, mobilenet_v2
 
 FLAGS = flags.FLAGS
 flags.DEFINE_boolean('train', True, 'Specify whether to train or evaluate a model.')
@@ -29,20 +29,35 @@ def main(argv):
     # TensorSpec(shape=(), dtype=tf.int64, name=None))>
     # Images: 256x256x3; Label: 0/1 (0 -> noDR, 1 -> DR)
     ds_train, ds_val, ds_test = datasets.load()
+    ds_info = 0 # Helper
 
-    # model
-    model = vgg_like(input_shape=ds_info.features["image"].shape, n_classes=ds_info.features["label"].num_classes)
+    '''
+    # HELPER: Printing of shapes
+    for idx, (data, labels) in enumerate(ds_train):
+        print(data.shape, labels.shape)
+        break
+    '''
+
+    # define model
+    model_name = 'LeNet'
+    if model_name == 'LeNet':
+        model = le_net(input_shape=(256, 256, 3), n_classes=2)
+    elif model_name == 'VGG16':
+        model = vgg16(input_shape=(256, 256, 3), n_classes=2)
+    elif model_name == 'MobileNetV2':
+        model = mobilenet_v2(input_shape=(256, 256, 3), n_classes=2)
 
     if FLAGS.train:
         trainer = Trainer(model, ds_train, ds_val, ds_info, run_paths)
         for _ in trainer.train():
             continue
     else:
+        '''
         evaluate(model,
                  checkpoint,
                  ds_test,
                  ds_info,
                  run_paths)
-
+        '''
 if __name__ == "__main__":
     app.run(main)
