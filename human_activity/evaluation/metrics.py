7b4d0ca6502b3a8ea49386b7887b5449eec5ea8f
FlorianSchatz <st191622@stud.uni-stuttgart.de>
2024-12-10 09:45:39 +0100

    Added categorical accuracy for one hot encoded data

--- human_activity/evaluation/metrics.py
+++ human_activity/evaluation/metrics.py
@@ -12,19 +12,22 @@ class ConfusionMatrix(tf.keras.metrics.Metric):
     def result(self):
         return None
 
-class Accuracy(tf.keras.metrics.Metric):
+class Categorical_Accuracy(tf.keras.metrics.Metric):
     def __init__(self, name='accuracy', **kwargs):
-        super(Accuracy, self).__init__(name=name, **kwargs)
+        super(Categorical_Accuracy, self).__init__(name=name, **kwargs)
         # initialize parameters
-        self.accuracy = self.add_weight(name='accuracy', initializer='zeros')
+        self.accuracy = self.add_weight(name='categorical_accuracy', initializer='zeros')
 
     def update_state(self, labels, predictions, *args, **kwargs):
-        # update parameters
-        # self.predictions = tf.reshape(tf.argmax(predictions, axis=2), [-1])
-        # self.labels = tf.reshape(labels, [-1])
+        # get highest rated class
+        labels = tf.argmax(labels, axis=-1)
+        predictions = tf.argmax(predictions, axis=-1)
 
-        self.accuracy = tf.math.reduce_mean(
-            tf.cast(tf.math.equal(self.labels, self.predictions), dtype=tf.float32))
+        # compare true and predicted
+        acc = tf.cast(tf.equal(labels, predictions), dtype=tf.float32)
+
+        # reduce to one value over all timesteps and entries
+        self.accuracy = tf.reduce_mean(acc)
 
     def result(self):
         # return parameters
