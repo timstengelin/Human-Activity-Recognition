38e0bea6c5b0e451b348a40f424d064064df85bd
FlorianSchatz <st191622@stud.uni-stuttgart.de>
2024-12-10 10:27:45 +0100

    Basic accuracy added and working

--- human_activity/evaluation/eval.py
+++ human_activity/evaluation/eval.py
@@ -30,65 +30,25 @@ def evaluate(model, ds_test, run_paths, n_classes):
                   loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
                   metrics=["accuracy"])#, [ConfusionMatrix(num_categories=num_categories)]])
 
-    print(model.summary())
+    accuracy = metrics.Categorical_Accuracy()
+    for data, label in ds_test:
+        y_pred = model(data)
+        accuracy.update_state(label, y_pred)
+        print(accuracy.result().numpy())
+        break
+    '''
+        for batch in ds_test.take(1):  # Nimm einen Batch aus dem Dataset
+        print(batch[0].shape)  # Überprüfe die Form der Eingabedaten
+        print(batch[1].shape)
+    '''
 
+    # TODO: Not working
     # evaluate the model
-    result = model.evaluate(ds_test, return_dict=True, batch_size=64)
+    # result = model.evaluate(ds_test, return_dict=False, steps=20)
 
     # log the evaluation information
-    logging.info(f"Evaluating at step: {step}...")
+    # logging.info(f"Evaluating at step: {step}...")
     # for key, value in result.items():
     #     logging.info('{}:\n{}'.format(key, value))
 
-    print(result)
-
-    # for idx, (test_data, test_labels) in enumerate(ds_test):
-    #     predictions = model(test_data, training=False)
-    #     predictions = tf.argmax(predictions, axis=2)
-    #     predictions = np.concatenate(predictions.numpy()).flatten()
-    #     test_labels = np.concatenate(test_labels.numpy()).flatten()
-    #
-    #     # postprocess the predictions by using the median filter or percentile filter
-    #     # (also compare the results of filter and choose the best)
-    #     plt.figure(dpi=800)
-    #     plt.title('POSTPROCESSING METHODS COMPARISON')
-    #     plt.xlabel('FILTER SIZE')
-    #     plt.ylabel('ACCURACY(%)')
-    #     plt.grid(b=True, axis='y')
-    #     for percentile in range(45, 60, 5):
-    #         size_list = range(0, 255, 10)
-    #         acc_list = []
-    #         for size in size_list:
-    #             if size != 0:
-    #                 test_predictions = percentile_filter(predictions, percentile=percentile, size=size)
-    #             else:
-    #                 test_predictions = predictions
-    #             test_accuracy = metrics.accuracy_score(test_labels, test_predictions) * 100
-    #             logging.info('accuracy(percentile filter {} with size {}):\n{}'.format(percentile, size, test_accuracy))
-    #             acc_list.append(test_accuracy)
-    #         if percentile == 50:
-    #             plt.plot(size_list, acc_list, marker="s", markersize=3, label=(str(percentile) + '%' + ' Percentile Filter(Median Filter)'))
-    #         else:
-    #             plt.plot(size_list, acc_list, marker="s", markersize=3, label=(str(percentile) + '%' + ' Percentile Filter'))
-    #     plt.legend(loc='lower left')
-    #     plt.savefig(run_paths['path_model_id'] + '/logs/eval/postprocessing_plot.png', dpi=800)
-    #     plt.show()
-    #
-    #     # plot the confusion matrix
-    #     cm = result['confusion_matrix']
-    #     fig, ax = plt.subplots()
-    #     im = ax.imshow(cm + 1, norm=colors.LogNorm(vmin=100, vmax=cm.max()), cmap='Wistia')
-    #     cbar = ax.figure.colorbar(im, ax=ax)
-    #     cbar.ax.set_ylabel('NUMBER OF SAMPLING POINTS', rotation=-90, va="bottom")
-    #     ax.set_xticks(np.arange(num_categories))
-    #     ax.set_yticks(np.arange(num_categories))
-    #     ax.set_xticklabels(['W', 'WU', 'WD', 'SI', 'ST', 'L', 'ST2SI', 'SI2ST', 'SI2L', 'L2SI', 'ST2L', 'L2ST'])
-    #     ax.set_yticklabels(['W', 'WU', 'WD', 'SI', 'ST', 'L', 'ST2SI', 'SI2ST', 'SI2L', 'L2SI', 'ST2L', 'L2ST'])
-    #     plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-    #     plt.setp(ax.get_yticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-    #     for i in range(num_categories):
-    #         for j in range(num_categories):
-    #             text = ax.text(j, i, cm[i, j], fontsize='x-small', ha="center", va="center", color="b")
-    #     ax.set_title("SEQUENCE TO SEQUENCE CONFUSION MATRIX")
-    #     fig.tight_layout()
-    #     plt.show()
+    # print(result)
