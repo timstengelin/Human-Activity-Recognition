9116378854cec70ea54c38a00537fd7035733f4a
FlorianSchatz <st191622@stud.uni-stuttgart.de>
2024-12-13 12:09:42 +0100

    visualization functional fertig

--- human_activity/evaluation/visualization.py
+++ human_activity/evaluation/visualization.py
@@ -36,9 +36,9 @@ def visulization(model, run_paths, dataset):
         label = tf.argmax(label, axis=-1)
 
         # append all prediction/true classes and data from dataset/batch to have one timeline
-        prediction = np.concatenate(prediction.numpy())
-        label = np.concatenate(label.numpy())
-        window = np.concatenate(window.numpy())
+        prediction = np.concatenate(prediction.numpy()[0::2])
+        label = np.concatenate(label.numpy()[0::2])
+        window = np.concatenate(window.numpy()[0::2])
 
         # pred_list.append(prediction)
         # label_list.append(label)
@@ -65,13 +65,13 @@ def visulization(model, run_paths, dataset):
     # Filtering of the classes to smooth out small mistakes
     prediction = median_filter(prediction, size=140)
 
-    def legending(labels):
+    def legending(labels, axis):
         """define the color map corresponding to each label"""
         label_color = ['dimgrey', 'darkorange', 'limegreen', 'royalblue', 'lightcoral', 'gold',
                         'aquamarine', 'mediumslateblue', 'saddlebrown', 'chartreuse', 'skyblue', 'violet']
         start = 0
-        for i in range(1, int(labels.size)):
-            plt.axvspan(i-1.5, i-0.5, facecolor=label_color[labels[i-1]], alpha=0.5)
+        for i in range(1, int(labels.size)+1):
+            axis.axvspan(i-1.5, i-0.5, facecolor=label_color[labels[i-1]], alpha=0.5)
     def labeling(labels):
         """define the color map corresponding to each label"""
         label_color = ['dimgrey', 'darkorange', 'limegreen', 'royalblue', 'lightcoral', 'gold',
@@ -85,7 +85,7 @@ def visulization(model, run_paths, dataset):
         plt.axvspan(start, int(labels.size) - 1, facecolor=label_color[labels[-1]], alpha=0.5)
 
     # limit sequence, that is shown from available dataset
-    range_time = [3000, 4000]
+    range_time = [1000, 4000]
     acc_x, acc_y, acc_z = (acc_x[range_time[0]:range_time[1]], acc_y[range_time[0]:range_time[1]],
                            acc_z[range_time[0]:range_time[1]])
     gyro_x, gyro_y, gyro_z = (gyro_x[range_time[0]:range_time[1]], gyro_y[range_time[0]:range_time[1]],
@@ -94,15 +94,18 @@ def visulization(model, run_paths, dataset):
     label = label[range_time[0]:range_time[1]]
 
     # plot the data as well as  their predictions and labels (the first figure is generated from predicted value, the second is the true distribution)
-    plt.figure(figsize=(12, 9), dpi=300)
+    # plt.figure(figsize=(12, 9), dpi=300)
 
     # plot prediction coming from model
-    ax11 = plt.subplot(3, 1, 1)
-    plt.title(label='PREDICTIONS', fontdict={'weight': 'normal', 'size': 'x-large'})
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1,figsize=(12,9), height_ratios=[3,3,1])
+                                        #gridspec_kw={"width_ratios": [3, 3],
+                                        #"height_ratios": [3, 3], "wspace" : 0.4, "hspace" : 0.4})
+    # ax11 = plt.subplot(3, 1, 1)
+    ax1.set_title(label='PREDICTIONS', fontdict={'weight': 'normal', 'size': 'x-large'})
     plt.tick_params(labelsize='x-small')
-    ax11.set_xlabel("TIME SEQUENCE")
-    ax11.set_ylabel("NORMALIZED LINEAR ACCELERATION")
-    ax12 = ax11.twinx() # same x-axis for other y-data
+    ax1.set_xlabel("TIME SEQUENCE")
+    ax1.set_ylabel("NORMALIZED LINEAR ACCELERATION")
+    ax12 = ax1.twinx() # same x-axis for other y-data
     ax12.set_ylabel("NORMALIZED AUGULAR VELOCITY")
     plt.plot(acc_x, label='acc_x', linewidth=1)
     plt.plot(acc_y, label='acc_y', linewidth=1)
@@ -111,12 +114,12 @@ def visulization(model, run_paths, dataset):
     plt.plot(gyro_y, label='gyro_y', linewidth=1)
     plt.plot(gyro_z, label='gyro_z', linewidth=1)
     labeling(prediction)
-    ax21 = plt.subplot(3, 1, 2)
-    plt.title('TRUE VALUES', fontdict={'weight': 'normal', 'size': 'x-large'})
+    # ax21 = plt.subplot(3, 1, 2)
+    ax2.set_title('TRUE VALUES', fontdict={'weight': 'normal', 'size': 'x-large'})
     plt.tick_params(labelsize='x-small')
-    ax21.set_xlabel("TIME SEQUENCE")
-    ax21.set_ylabel("NORMALIZED LINEAR ACCELERATION")
-    ax22 = ax21.twinx()
+    ax2.set_xlabel("TIME SEQUENCE")
+    ax2.set_ylabel("NORMALIZED LINEAR ACCELERATION")
+    ax22 = ax2.twinx()
     ax22.set_ylabel("NORMALIZED AUGULAR VELOCITY")
     plt.plot(acc_x, label='acc_x', linewidth=1)
     plt.plot(acc_y, label='acc_y', linewidth=1)
@@ -125,17 +128,19 @@ def visulization(model, run_paths, dataset):
     plt.plot(gyro_y, label='gyro_y', linewidth=1)
     plt.plot(gyro_z, label='gyro_z', linewidth=1)
     labeling(label)
-    ax31 = plt.subplot(3, 1, 3)
-    ax31.set_xlabel("CLASSES")
-    legending(np.array([0,1,2,3,4,5,6,7,8,9,10,11]))
-    plt.yticks([])
+    # ax31 = plt.subplot(3, 1, 3)
+    legending(np.array([0,1,2,3,4,5,6,7,8,9,10,11]), ax3)
+    ax3.set_yticks([])
     positions = [0,1,2,3,4,5,6,7,8,9,10,11]
     labels = ["WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING",
               "STAND_TO_SIT", "SIT_TO_STAND", "SIT_TO_LIE", "LIE_TO_SIT", "STAND_TO_LIE", "LIE_TO_STAND"]
-    ax31.xaxis.set_major_locator(matplotlib.ticker.FixedLocator(positions))
-    ax31.xaxis.set_major_formatter(matplotlib.ticker.FixedFormatter(labels))
-    plt.figure(figsize=(12, 1))
-    plt.xticks(rotation=70)
+    ax3.xaxis.set_major_locator(matplotlib.ticker.FixedLocator(positions))
+    ax3.xaxis.set_major_formatter(matplotlib.ticker.FixedFormatter(labels))
+    # plt.figure(figsize=(12, 1))
+    ax3.tick_params(axis='x', labelrotation=45)
+    ax1.margins(0,0)
+    ax2.margins(0, 0)
+    ax3.margins(0, 0)
     plt.tight_layout()
     plt.savefig('plot.png', dpi=150)
     plt.show()
