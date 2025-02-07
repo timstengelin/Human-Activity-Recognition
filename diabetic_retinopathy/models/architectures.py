153960d802b111f840dbb569e3930962b08d65d9
Tim Stengelin <st190770@stud.uni-stuttgart.de>
2025-02-08 00:12:56 +0100

    Checked for pep8 (auto.) in architectures.py

--- diabetic_retinopathy/models/architectures.py
+++ diabetic_retinopathy/models/architectures.py
@@ -5,12 +5,17 @@ from models.layers import *
 
 
 @gin.configurable
-def mobilenet_v2(input_shape=(256, 256, 3), n_classes=2, alpha=1.0, dropout_rate=0.2):
+def mobilenet_v2(
+        input_shape=(
+            256,
+            256,
+            3), n_classes=2, alpha=1.0, dropout_rate=0.2):
     '''
     Defines the MobileNetV2 architecture
 
     Args:
-        input_shape (tuple): Shape of the input tensor (height, width, channels)
+        input_shape (tuple): Shape of the input tensor (height, width,
+            channels)
         n_classes (int): Number of output classes
         alpha (float): Width multiplier to control the number of filters
         dropout_rate (float): Dropout rate for the top layer
@@ -26,21 +31,77 @@ def mobilenet_v2(input_shape=(256, 256, 3), n_classes=2, alpha=1.0, dropout_rate
     x = tf.keras.layers.Rescaling(1.0 / 255.0)(inputs)
 
     # Initial Conv2D layer
-    x = tf.keras.layers.Conv2D(round_filters(32, alpha), kernel_size=3, strides=2, padding='same', use_bias=False)(x)
+    x = tf.keras.layers.Conv2D(
+        round_filters(
+            32,
+            alpha),
+        kernel_size=3,
+        strides=2,
+        padding='same',
+        use_bias=False)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU(6.0)(x)
 
     # Bottleneck blocks
-    x = bottleneck_block(x, filters=16, expansion_factor=1, stride=1, repeats=1, alpha=alpha)
-    x = bottleneck_block(x, filters=24, expansion_factor=6, stride=2, repeats=2, alpha=alpha)
-    x = bottleneck_block(x, filters=32, expansion_factor=6, stride=2, repeats=3, alpha=alpha)
-    x = bottleneck_block(x, filters=64, expansion_factor=6, stride=2, repeats=4, alpha=alpha)
-    x = bottleneck_block(x, filters=96, expansion_factor=6, stride=1, repeats=3, alpha=alpha)
-    x = bottleneck_block(x, filters=160, expansion_factor=6, stride=2, repeats=3, alpha=alpha)
-    x = bottleneck_block(x, filters=320, expansion_factor=6, stride=1, repeats=1, alpha=alpha)
+    x = bottleneck_block(
+        x,
+        filters=16,
+        expansion_factor=1,
+        stride=1,
+        repeats=1,
+        alpha=alpha)
+    x = bottleneck_block(
+        x,
+        filters=24,
+        expansion_factor=6,
+        stride=2,
+        repeats=2,
+        alpha=alpha)
+    x = bottleneck_block(
+        x,
+        filters=32,
+        expansion_factor=6,
+        stride=2,
+        repeats=3,
+        alpha=alpha)
+    x = bottleneck_block(
+        x,
+        filters=64,
+        expansion_factor=6,
+        stride=2,
+        repeats=4,
+        alpha=alpha)
+    x = bottleneck_block(
+        x,
+        filters=96,
+        expansion_factor=6,
+        stride=1,
+        repeats=3,
+        alpha=alpha)
+    x = bottleneck_block(
+        x,
+        filters=160,
+        expansion_factor=6,
+        stride=2,
+        repeats=3,
+        alpha=alpha)
+    x = bottleneck_block(
+        x,
+        filters=320,
+        expansion_factor=6,
+        stride=1,
+        repeats=1,
+        alpha=alpha)
 
     # Last Conv2D layer
-    x = tf.keras.layers.Conv2D(round_filters(1280, alpha), kernel_size=1, strides=1, padding='same', use_bias=False)(x)
+    x = tf.keras.layers.Conv2D(
+        round_filters(
+            1280,
+            alpha),
+        kernel_size=1,
+        strides=1,
+        padding='same',
+        use_bias=False)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU(6.0)(x)
 
@@ -50,19 +111,27 @@ def mobilenet_v2(input_shape=(256, 256, 3), n_classes=2, alpha=1.0, dropout_rate
     # Global average pooling, dropout, and dense output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
-    outputs = tf.keras.layers.Dense(n_classes, activation='softmax', name='output')(x)
+    outputs = tf.keras.layers.Dense(
+        n_classes,
+        activation='softmax',
+        name='output')(x)
 
     return tf.keras.Model(inputs=inputs, outputs=outputs, name='mobilenet_v2')
 
 
-
 @gin.configurable
-def efficientnet_b0(input_shape=(256, 256, 3), n_classes=2, width_coefficient=1.0, depth_coefficient=1.0, dropout_rate=0.2):
+def efficientnet_b0(
+        input_shape=(
+            256,
+            256,
+            3), n_classes=2, width_coefficient=1.0, depth_coefficient=1.0,
+        dropout_rate=0.2):
     '''
     Defines the EfficientNetB0 architecture
 
     Args:
-        input_shape (tuple): Shape of the input tensor (height, width, channels)
+        input_shape (tuple):
+            Shape of the input tensor (height, width, channels)
         n_classes (int): Number of output classes
         width_coefficient (float): Width scaling factor
         depth_coefficient (float): Depth scaling factor
@@ -82,21 +151,91 @@ def efficientnet_b0(input_shape=(256, 256, 3), n_classes=2, width_coefficient=1.
     x = tf.keras.layers.Rescaling(1.0 / 255.0)(inputs)
 
     # Initial Conv2D layer
-    x = tf.keras.layers.Conv2D(round_filters(32, width_coefficient), kernel_size=3, strides=2, padding='same', use_bias=False)(x)
+    x = tf.keras.layers.Conv2D(
+        round_filters(
+            32,
+            width_coefficient),
+        kernel_size=3,
+        strides=2,
+        padding='same',
+        use_bias=False)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU()(x)
 
     # MBConv blocks
-    x = mbconv_block(x, filters=16, expansion_factor=1, stride=1, repeats=round_repeats(1, depth_coefficient), width_coefficient=width_coefficient)
-    x = mbconv_block(x, filters=24, expansion_factor=6, stride=2, repeats=round_repeats(2, depth_coefficient), width_coefficient=width_coefficient)
-    x = mbconv_block(x, filters=40, expansion_factor=6, stride=2, repeats=round_repeats(2, depth_coefficient), width_coefficient=width_coefficient)
-    x = mbconv_block(x, filters=80, expansion_factor=6, stride=2, repeats=round_repeats(3, depth_coefficient), width_coefficient=width_coefficient)
-    x = mbconv_block(x, filters=112, expansion_factor=6, stride=1, repeats=round_repeats(3, depth_coefficient), width_coefficient=width_coefficient)
-    x = mbconv_block(x, filters=192, expansion_factor=6, stride=2, repeats=round_repeats(4, depth_coefficient), width_coefficient=width_coefficient)
-    x = mbconv_block(x, filters=320, expansion_factor=6, stride=1, repeats=round_repeats(1, depth_coefficient), width_coefficient=width_coefficient)
+    x = mbconv_block(
+        x,
+        filters=16,
+        expansion_factor=1,
+        stride=1,
+        repeats=round_repeats(
+            1,
+            depth_coefficient),
+        width_coefficient=width_coefficient)
+    x = mbconv_block(
+        x,
+        filters=24,
+        expansion_factor=6,
+        stride=2,
+        repeats=round_repeats(
+            2,
+            depth_coefficient),
+        width_coefficient=width_coefficient)
+    x = mbconv_block(
+        x,
+        filters=40,
+        expansion_factor=6,
+        stride=2,
+        repeats=round_repeats(
+            2,
+            depth_coefficient),
+        width_coefficient=width_coefficient)
+    x = mbconv_block(
+        x,
+        filters=80,
+        expansion_factor=6,
+        stride=2,
+        repeats=round_repeats(
+            3,
+            depth_coefficient),
+        width_coefficient=width_coefficient)
+    x = mbconv_block(
+        x,
+        filters=112,
+        expansion_factor=6,
+        stride=1,
+        repeats=round_repeats(
+            3,
+            depth_coefficient),
+        width_coefficient=width_coefficient)
+    x = mbconv_block(
+        x,
+        filters=192,
+        expansion_factor=6,
+        stride=2,
+        repeats=round_repeats(
+            4,
+            depth_coefficient),
+        width_coefficient=width_coefficient)
+    x = mbconv_block(
+        x,
+        filters=320,
+        expansion_factor=6,
+        stride=1,
+        repeats=round_repeats(
+            1,
+            depth_coefficient),
+        width_coefficient=width_coefficient)
 
     # Last Conv2D layer
-    x = tf.keras.layers.Conv2D(round_filters(1280, width_coefficient), kernel_size=1, strides=1, padding='same', use_bias=False)(x)
+    x = tf.keras.layers.Conv2D(
+        round_filters(
+            1280,
+            width_coefficient),
+        kernel_size=1,
+        strides=1,
+        padding='same',
+        use_bias=False)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU()(x)
 
@@ -106,20 +245,32 @@ def efficientnet_b0(input_shape=(256, 256, 3), n_classes=2, width_coefficient=1.
     # Global average pooling, dropout, and dense output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
-    outputs = tf.keras.layers.Dense(n_classes, activation='softmax', name='output')(x)
+    outputs = tf.keras.layers.Dense(
+        n_classes,
+        activation='softmax',
+        name='output')(x)
 
-    return tf.keras.Model(inputs=inputs, outputs=outputs, name='efficientnet_b0')
+    return tf.keras.Model(
+        inputs=inputs,
+        outputs=outputs,
+        name='efficientnet_b0')
 
 
 @gin.configurable
-def efficientnet_b3_pretrained(input_shape=(256, 256, 3), n_classes=2, trainable_rate=0.2, dropout_rate=0.2):
+def efficientnet_b3_pretrained(
+        input_shape=(
+            256,
+            256,
+            3), n_classes=2, trainable_rate=0.2, dropout_rate=0.2):
     '''
     Defines a pretrained EfficientNetB3 architecture
 
     Args:
-        input_shape (tuple): Shape of the input tensor (height, width, channels)
+        input_shape (tuple):
+            Shape of the input tensor (height, width, channels)
         n_classes (int): Number of output classes
-        trainable_rate (float): proportion of trainable parameters in the feature extraction module
+        trainable_rate (float): proportion of trainable parameters in the
+            feature extraction module
         dropout_rate (float): Dropout rate for the top layer
 
     Returns:
@@ -130,14 +281,20 @@ def efficientnet_b3_pretrained(input_shape=(256, 256, 3), n_classes=2, trainable
     inputs = tf.keras.Input(shape=input_shape)
 
     # Preprocess input data
-    preprocessed_inputs = tf.keras.applications.efficientnet.preprocess_input(inputs)
+    preprocessed_inputs = tf.keras.applications.efficientnet.preprocess_input(
+        inputs)
 
     # Build EfficientNetB3 model
-    base_model = tf.keras.applications.EfficientNetB3(include_top=False, weights='imagenet', input_shape=input_shape,
-                                                      pooling=None, classifier_activation=None)
+    base_model = tf.keras.applications.EfficientNetB3(
+        include_top=False,
+        weights='imagenet',
+        input_shape=input_shape,
+        pooling=None,
+        classifier_activation=None)
 
     # Determine layers to freeze
-    last_nontrainable_layer = len(base_model.layers) - int(len(base_model.layers) * trainable_rate)
+    last_nontrainable_layer = len(
+        base_model.layers) - int(len(base_model.layers) * trainable_rate)
 
     # Freeze layers up to the fine-tune index
     for lay in base_model.layers[:last_nontrainable_layer]:
@@ -152,20 +309,32 @@ def efficientnet_b3_pretrained(input_shape=(256, 256, 3), n_classes=2, trainable
     # Global average pooling and dense output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
-    outputs = tf.keras.layers.Dense(n_classes, activation='softmax', name='output')(x)
+    outputs = tf.keras.layers.Dense(
+        n_classes,
+        activation='softmax',
+        name='output')(x)
 
-    return tf.keras.Model(inputs=inputs, outputs=outputs, name='efficientnet_b3_pretrained')
+    return tf.keras.Model(
+        inputs=inputs,
+        outputs=outputs,
+        name='efficientnet_b3_pretrained')
 
 
 @gin.configurable
-def densenet201_pretrained(input_shape=(256, 256, 3), n_classes=2, trainable_rate=0.2, dropout_rate=0.2):
+def densenet201_pretrained(
+        input_shape=(
+            256,
+            256,
+            3), n_classes=2, trainable_rate=0.2, dropout_rate=0.2):
     '''
     Defines a pretrained DenseNet201 architecture
 
     Args:
-        input_shape (tuple): Shape of the input tensor (height, width, channels)
+        input_shape (tuple): Shape of the input tensor
+            (height, width, channels)
         n_classes (int): Number of output classes
-        trainable_rate (float): proportion of trainable parameters in the feature extraction module
+        trainable_rate (float): proportion of trainable parameters in the
+            feature extraction module
         dropout_rate (float): Dropout rate for the top layer
 
     Returns:
@@ -176,14 +345,20 @@ def densenet201_pretrained(input_shape=(256, 256, 3), n_classes=2, trainable_rat
     inputs = tf.keras.Input(shape=input_shape)
 
     # Preprocess input data
-    preprocessed_inputs = tf.keras.applications.densenet.preprocess_input(inputs)
+    preprocessed_inputs = tf.keras.applications.densenet.preprocess_input(
+        inputs)
 
     # Build DenseNet201 model
-    base_model = tf.keras.applications.DenseNet201(include_top=False, weights='imagenet', input_shape=input_shape,
-                                                   pooling=None, classifier_activation=None)
+    base_model = tf.keras.applications.DenseNet201(
+        include_top=False,
+        weights='imagenet',
+        input_shape=input_shape,
+        pooling=None,
+        classifier_activation=None)
 
     # Determine layers to freeze
-    last_nontrainable_layer = len(base_model.layers) - int(len(base_model.layers) * trainable_rate)
+    last_nontrainable_layer = len(
+        base_model.layers) - int(len(base_model.layers) * trainable_rate)
 
     # Freeze layers up to the fine-tune index
     for lay in base_model.layers[:last_nontrainable_layer]:
@@ -198,28 +373,46 @@ def densenet201_pretrained(input_shape=(256, 256, 3), n_classes=2, trainable_rat
     # Global average pooling and dense output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
-    outputs = tf.keras.layers.Dense(n_classes, activation='softmax', name='output')(x)
+    outputs = tf.keras.layers.Dense(
+        n_classes,
+        activation='softmax',
+        name='output')(x)
 
-    return tf.keras.Model(inputs=inputs, outputs=outputs, name='densenet201_pretrained')
+    return tf.keras.Model(
+        inputs=inputs,
+        outputs=outputs,
+        name='densenet201_pretrained')
 
 
 @gin.configurable
-def mobilenet_v2_AND_efficientnet_b0_AND_efficientnet_b3_AND_densenet201(input_shape=(256, 256, 3)):
+def mobilenet_v2_AND_efficientnet_b0_AND_efficientnet_b3_AND_densenet201(
+    input_shape=(
+        256,
+        256,
+        3)):
     '''
-    Defines an ensemble of MobileNetV2, EfficientNetB0, EfficientNetB3 and DenseNet201
+    Defines an ensemble of MobileNetV2, EfficientNetB0,
+        EfficientNetB3 and DenseNet201
 
     Args:
-        input_shape (tuple): Shape of the input tensor (height, width, channels)
+        input_shape (tuple): Shape of the input
+            tensor (height, width, channels)
 
     Returns:
         (tf.keras.Model): ensemble model
     '''
 
     # Define models
-    models = [mobilenet_v2(input_shape=(256, 256, 3), n_classes=2),
-              efficientnet_b0(input_shape=(256, 256, 3), n_classes=2),
-              efficientnet_b3_pretrained(input_shape=(256, 256, 3), n_classes=2),
-              densenet201_pretrained(input_shape=(256, 256, 3), n_classes=2)]
+    models = [
+        mobilenet_v2(
+            input_shape=(
+                256, 256, 3), n_classes=2), efficientnet_b0(
+            input_shape=(
+                256, 256, 3), n_classes=2), efficientnet_b3_pretrained(
+            input_shape=(
+                256, 256, 3), n_classes=2), densenet201_pretrained(
+            input_shape=(
+                256, 256, 3), n_classes=2)]
 
     # Input layer of ensemble model
     ensemble_input = tf.keras.Input(shape=input_shape)
@@ -233,9 +426,12 @@ def mobilenet_v2_AND_efficientnet_b0_AND_efficientnet_b3_AND_densenet201(input_s
     # Determine the final class based on argmax (voting principle)
     final_classes = tf.argmax(mean_probs, axis=1)
 
-    # Convert the final classes to probabilities (100% for the chosen class, 0% for the other class)
+    # Convert the final classes to probabilities (100% for the chosen class,
+    # 0% for the other class)
     combined_output = tf.one_hot(final_classes, depth=2, dtype=tf.float32)
 
-    return tf.keras.Model(inputs=ensemble_input,
-                          outputs=combined_output,
-                          name='mobilenet_v2_AND_efficientnet_b0_AND_efficientnet_b3_AND_densenet201')
+    return tf.keras.Model(
+        inputs=ensemble_input,
+        outputs=combined_output,
+        name='mobilenet_v2_AND_efficientnet_b0_'
+             'AND_efficientnet_b3_AND_densenet201')
