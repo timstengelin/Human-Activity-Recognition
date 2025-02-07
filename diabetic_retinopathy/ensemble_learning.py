50ccebf056904d3b0f0aa6b9d3d1da7a8a44f6ce
Tim Stengelin <st190770@stud.uni-stuttgart.de>
2025-02-07 23:46:59 +0100

    Checked for pep8 in ensemble_learning.py

--- diabetic_retinopathy/ensemble_learning.py
+++ diabetic_retinopathy/ensemble_learning.py
@@ -16,7 +16,8 @@ def create_ensemble_model(models, run_paths):
     '''
 
     path_model_root = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), os.pardir, os.pardir, '05_Workspace', 'experiments'))
+        os.path.dirname(__file__), os.pardir, os.pardir, '05_Workspace',
+        'experiments'))
     helper_path_list = [
         os.path.join(path_model_root, 'mobilenetv2_withAug', 'ckpts'),
         os.path.join(path_model_root, 'efficientnetb0_withAug', 'ckpts'),
@@ -32,13 +33,17 @@ def create_ensemble_model(models, run_paths):
 
     # Restore Checkpoints for models
     for idx, model in enumerate(models):
-        ckpts.append(tf.train.Checkpoint(step=tf.Variable(1), optimizer=tf.keras.optimizers.Adam(), net=model))
+        ckpts.append(tf.train.Checkpoint(step=tf.Variable(1),
+                                         optimizer=tf.keras.optimizers.Adam(),
+                                         net=model))
         ckpt_managers.append(tf.train.CheckpointManager(checkpoint=ckpts[-1],
-                                                        directory=helper_path_list[idx],
+                                                        directory=
+                                                        helper_path_list[idx],
                                                         max_to_keep=1))
         ckpts[-1].restore(ckpt_managers[-1].latest_checkpoint).expect_partial()
 
-        logging.info("Restored from {}".format(ckpt_managers[-1].latest_checkpoint))
+        logging.info("Restored from"
+                     "{}".format(ckpt_managers[-1].latest_checkpoint))
 
     # Input layer of ensemble model
     ensemble_input = tf.keras.Input(shape=(256, 256, 3))
@@ -52,11 +57,13 @@ def create_ensemble_model(models, run_paths):
     # Determine the final class based on argmax (voting principle)
     final_classes = tf.argmax(mean_probs, axis=1)
 
-    # Convert the final classes to probabilities (100% for the chosen class, 0% for the other class)
+    # Convert the final classes to probabilities (100% for the chosen class,
+    # 0% for the other class)
     combined_output = tf.one_hot(final_classes, depth=2, dtype=tf.float32)
 
     # Define the final ensemble model
-    ensemble_model = tf.keras.Model(inputs=ensemble_input, outputs=combined_output)
+    ensemble_model = tf.keras.Model(inputs=ensemble_input,
+                                    outputs=combined_output)
     ensemble_model.compile(
         optimizer=tf.keras.optimizers.Adam(),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
@@ -67,9 +74,12 @@ def create_ensemble_model(models, run_paths):
     ensemble_model.summary()
 
     # Checkpoint manager for ensemble model
-    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=tf.keras.optimizers.Adam(), net=ensemble_model)
+    ckpt = tf.train.Checkpoint(step=tf.Variable(1),
+                               optimizer=tf.keras.optimizers.Adam(),
+                               net=ensemble_model)
     ckpt_manager = tf.train.CheckpointManager(checkpoint=ckpt,
-                                              directory=run_paths["path_ckpts_train"],
+                                              directory=
+                                              run_paths["path_ckpts_train"],
                                               max_to_keep=1)
 
     # Save final checkpoint of ensemble model
