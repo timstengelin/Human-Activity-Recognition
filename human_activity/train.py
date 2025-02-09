ffaef473f7ffc4d5e9821deae3689487a2b8b23e
FlorianSchatz <st191622@stud.uni-stuttgart.de>
2025-02-09 09:44:34 +0100

    PEP8 for train.py

--- human_activity/train.py
+++ human_activity/train.py
@@ -1,3 +1,4 @@
+"""This file impellements the whole training process class for the model."""
 import gin
 import tensorflow as tf
 import logging
@@ -9,10 +10,28 @@ from input_pipeline import datasets
 import models.architectures as architectures
 from evaluation.metrics import crossentropy
 
+
 @gin.configurable
 class Trainer(object):
+    """Create an object Trainer for model training."""
+
     def __init__(self, model, ds_train, ds_val, learning_rate, run_paths,
-                 total_steps, log_interval, ckpt_interval, class_weight, tuning=False):
+                 total_steps, log_interval, ckpt_interval, class_weight,
+                 tuning=False):
+        """Initialize the Trainer.
+
+        Parameters:
+            model (tf.model): model which should be trained
+            ds_train (tf.data.Dataset): training dataset
+            ds_val (tf.data.Dataset): validation dataset
+            learning_rate (float): learning rate
+            run_paths (dict): run paths with pathes
+            total_steps (int): total number of training steps
+            log_interval (int): log interval
+            ckpt_interval (int): checkpoint interval
+            class_weight (array): class weights for balancing
+            tuning (bool): whether the model is trained within tuning process
+        """
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
         # Metrics
@@ -35,25 +54,31 @@ class Trainer(object):
 
         # Checkpoint Manager
         if not tuning:
-            self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=self.optimizer, net=self.model)
-            self.ckpt_manager = tf.train.CheckpointManager(checkpoint=self.ckpt, directory=self.run_paths["path_ckpts_train"],
-                                                           max_to_keep=10)
+            self.ckpt = tf.train.Checkpoint(step=tf.Variable(1),
+                                            optimizer=self.optimizer,
+                                            net=self.model)
+            self.ckpt_manager = (
+                tf.train.CheckpointManager(
+                    checkpoint=self.ckpt,
+                    directory=self.run_paths["path_ckpts_train"],
+                    max_to_keep=10))
 
         # Summary Writer
         if not tuning:
-            self.train_summary_writer = tf.summary.create_file_writer(self.run_paths['path_board_train'])
-            self.val_summary_writer = tf.summary.create_file_writer(self.run_paths['path_board_val'])
+            self.train_summary_writer = (
+                tf.summary.create_file_writer(
+                    self.run_paths['path_board_train']))
+            self.val_summary_writer = (
+                tf.summary.create_file_writer(
+                    self.run_paths['path_board_val']))
 
     @tf.function
     def train_step(self, data, labels):
-        """A basic single LSTM layer
+        """Train a step with the model.
 
-            Parameters:
-                data (Tensor): input data for train step
-                labels (int): true labels for train step
-
-            Returns:
-                none
+        Parameters:
+            data (Tensor): input data for train step
+            labels (int): true labels for train step
         """
         with tf.GradientTape() as tape:
             # training=True is only needed if there are layers with different
@@ -61,21 +86,19 @@ class Trainer(object):
             predictions = self.model(data, training=True)
             loss = crossentropy(labels, predictions, self.class_weight)
         gradients = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        self.optimizer.apply_gradients(zip(gradients,
+                                           self.model.trainable_variables))
 
         self.train_loss(loss)
         self.train_accuracy(labels, predictions)
 
     @tf.function
     def val_step(self, data, labels):
-        """A basic single LSTM layer
+        """Validate a step with the model.
 
-            Parameters:
-                data (Tensor): input data for validation step
-                labels (int): true labels for validation step
-
-            Returns:
-                none
+        Parameters:
+            data (Tensor): input data for validation step
+            labels (int): true labels for validation step
         """
         # training=False is only needed if there are layers with different
         # behavior during training versus inference (e.g. Dropout).
@@ -86,12 +109,12 @@ class Trainer(object):
         self.val_accuracy(labels, predictions)
 
     def train(self):
-        """train function
+        """Train model with function.
 
-            Parameters:
-                none
-            Returns:
-                none
+        Parameters:
+            none
+        Returns:
+            none
         """
         # resume the model by continuing training if model is available
         if not self.tuning:
@@ -105,8 +128,11 @@ class Trainer(object):
             # Write summary to tensorboard
             if not self.tuning:
                 with self.train_summary_writer.as_default():
-                    tf.summary.scalar('loss', self.train_loss.result(), step=step)
-                    tf.summary.scalar('accuracy', self.train_accuracy.result() * 100, step=step)
+                    tf.summary.scalar('loss', self.train_loss.result(),
+                                      step=step)
+                    tf.summary.scalar('accuracy',
+                                      self.train_accuracy.result() * 100,
+                                      step=step)
 
             if step % self.log_interval == 0:
 
@@ -117,24 +143,29 @@ class Trainer(object):
                 for val_data, val_labels in self.ds_val:
                     self.val_step(val_data, val_labels)
 
-                template = 'Step {}, Loss: {:.6f}, Accuracy: {:.6f}, Validation Loss: {:.6f}, Validation Accuracy: {:.6f}'
+                template = ('Step {}, Loss: {:.6f}, Accuracy: {:.6f}, ' +
+                            'Validation Loss: {:.6f}, ' +
+                            'Validation Accuracy: {:.6f}')
                 logging.info(template.format(step,
                                              self.train_loss.result(),
                                              self.train_accuracy.result() * 100,
                                              self.val_loss.result(),
                                              self.val_accuracy.result() * 100))
-                
+
                 # Write summary to tensorboard
                 if not self.tuning:
                     with self.val_summary_writer.as_default():
-                        tf.summary.scalar('loss', self.val_loss.result(), step=step)
-                        tf.summary.scalar('accuracy', self.val_accuracy.result() * 100, step=step)
+                        tf.summary.scalar('loss', self.val_loss.result(),
+                                          step=step)
+                        tf.summary.scalar('accuracy',
+                                          self.val_accuracy.result() * 100,
+                                          step=step)
 
                 if self.tuning:
                     wandb.log({"acc_train": self.train_accuracy.result()*100,
                                "loss_train": self.train_loss.result(),
                                "acc_val": self.val_accuracy.result()*100,
-                               "loss_val": self.val_loss.result(),})
+                               "loss_val": self.val_loss.result()})
 
                 # Reset train metrics
                 self.train_loss.reset_states()
@@ -146,7 +177,8 @@ class Trainer(object):
                 # Save checkpoint
                 if not self.tuning:
                     self.ckpt_manager.save()
-                    logging.info(f'Saving checkpoint to {self.run_paths["path_ckpts_train"]}.')
+                    logging.info(f'Saving checkpoint to ' +
+                                 f'{self.run_paths["path_ckpts_train"]}.')
 
             if step % self.total_steps == 0:
                 # Save final checkpoint
@@ -155,22 +187,48 @@ class Trainer(object):
                     logging.info(f'Finished training after {step} steps.')
                 return self.val_accuracy.result().numpy()
 
+
 def create_model(model_name, feature_shape, label_shape):
+    """Create a model for the given name.
+
+    Parameters:
+        model_name (string): name of requested model
+        feature_shape (Tensor): desired shape of model
+        label_shape (Tensor): desired shape of labels
+
+    Returns:
+        model (tf.keras.Model): model according to name
+    """
     if model_name == "LSTM_model":
-        model = architectures.lstm_architecture(input_shape=feature_shape, n_classes=label_shape[-1])
+        model = architectures.lstm_architecture(input_shape=feature_shape,
+                                                n_classes=label_shape[-1])
     elif model_name == "bidi_LSTM_model":
-        model = architectures.bidi_lstm_architecture(input_shape=feature_shape, n_classes=label_shape[-1])
+        model = architectures.bidi_lstm_architecture(input_shape=feature_shape,
+                                                     n_classes=label_shape[-1])
     elif model_name == "GRU_model":
-        model = architectures.gru_architecture(input_shape=feature_shape, n_classes=label_shape[-1])
+        model = architectures.gru_architecture(input_shape=feature_shape,
+                                               n_classes=label_shape[-1])
     elif model_name == "RNN_model":
-        model = architectures.rnn_architecture(input_shape=feature_shape, n_classes=label_shape[-1])
+        model = architectures.rnn_architecture(input_shape=feature_shape,
+                                               n_classes=label_shape[-1])
     elif model_name == "Conv1d_model":
-        model = architectures.conv1d_architecture(input_shape=feature_shape, n_classes=label_shape[-1])
+        model = architectures.conv1d_architecture(input_shape=feature_shape,
+                                                  n_classes=label_shape[-1])
     elif model_name == "variable_LSTM_model":
-        model = architectures.variable_lstm_architecture(input_shape=feature_shape, n_classes=label_shape[-1])
+        model = (architectures.
+                 variable_lstm_architecture(input_shape=feature_shape,
+                                            n_classes=label_shape[-1]))
     return model
+
+
 @gin.configurable
 def training(run_paths, model_name):
+    """Create a model for the given name.
+
+    Parameters:
+        run_paths (dict): dict with pathes
+        model_name (string): desired name of model
+    """
     # set logger for training
     utils_misc.set_loggers(run_paths['path_logs_train'], logging.INFO)
 
@@ -185,9 +243,11 @@ def training(run_paths, model_name):
         label_shape = label.shape[1:]
         break
 
-    model = create_model(model_name=model_name, feature_shape=feature_shape, label_shape=label_shape)
+    model = create_model(model_name=model_name, feature_shape=feature_shape,
+                         label_shape=label_shape)
 
     # initialize Trainer class based on given model and datasets
-    trainer = Trainer(model=model, ds_train=ds_train, ds_val=ds_val, run_paths=run_paths)
+    trainer = Trainer(model=model, ds_train=ds_train, ds_val=ds_val,
+                      run_paths=run_paths)
     for _ in trainer.train():
         continue
