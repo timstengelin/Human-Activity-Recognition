503c6f5b64fcae555e9067e417cb4d1b45e2290f
FlorianSchatz <st191622@stud.uni-stuttgart.de>
2025-02-09 09:53:19 +0100

    PEP8 for tune.py

--- human_activity/tune.py
+++ human_activity/tune.py
@@ -1,16 +1,24 @@
+"""This file iomplements the tuning functionality."""
 import logging
 import gin
-
 from train import Trainer
 import input_pipeline.datasets as datasets
-import models.architectures as architectures
 from utils import utils_misc
 from train import create_model
-
 import wandb
+
 
 @gin.configurable
 def tune(run_paths, key, sweep_config, parameters_dict, n_runs):
+    """Tune models with wandb evaluation.
+
+    Parameters:
+        run_paths (string): dict with pathes
+        key (string): key for upload in wandb
+        sweep_config (dict): sweep config parameters for wandb
+        parameters_dict (dict): hyperparameters for tuning
+        n_runs (int): number of wandb runs
+    """
     utils_misc.set_loggers(run_paths['path_logs_tune'], logging.INFO)
     # wandb login with given key
     wandb.login(key=key)
@@ -18,11 +26,12 @@ def tune(run_paths, key, sweep_config, parameters_dict, n_runs):
     sweep_config['parameters'] = parameters_dict
 
     # create wandb conf and obj
-    sweep_id = wandb.sweep(sweep=sweep_config, project="Activity Recognition HAPT")
+    sweep_id = wandb.sweep(sweep=sweep_config,
+                           project="Activity Recognition HAPT")
 
-
-    #actual tuning function
+    # actual tuning function
     def tuning():
+        """Train one model with tuning parameters."""
         config = {
             'steps': 50,
             'lr_rate': 0.01,
@@ -33,15 +42,16 @@ def tune(run_paths, key, sweep_config, parameters_dict, n_runs):
             'units': 64
         }
         if config:
-            run = wandb.init(config=config)
+            wandb.init(config=config)
 
             config = wandb.config
 
             # Model training code here ...
-            ds_train, ds_val, ds_test, ds_info = datasets.load(window_size=config.window_size,
-                                                               window_shift=config.window_shift,
-                                                               batch_size=config.batch_size,
-                                                               tfrecord_files_exist=False)
+            ds_train, ds_val, ds_test, ds_info = (
+                datasets.load(window_size=config.window_size,
+                              window_shift=config.window_shift,
+                              batch_size=config.batch_size,
+                              tfrecord_files_exist=False))
 
             # get shape from actual dataset
             feature_shape = None
@@ -51,10 +61,13 @@ def tune(run_paths, key, sweep_config, parameters_dict, n_runs):
                 label_shape = label.shape[1:]
                 break
 
-            model = create_model(model_name=config.model, feature_shape=feature_shape, label_shape=label_shape)
+            model = create_model(model_name=config.model,
+                                 feature_shape=feature_shape,
+                                 label_shape=label_shape)
 
             trainer = Trainer(model=model, ds_train=ds_train, ds_val=ds_val,
-                              run_paths=run_paths, total_steps=config.steps, tuning=True, learning_rate=config.lr_rate)
+                              run_paths=run_paths, total_steps=config.steps,
+                              tuning=True, learning_rate=config.lr_rate)
             for _ in trainer.train():
                 continue
 
